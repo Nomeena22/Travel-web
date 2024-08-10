ad7765117a4087ff2ff02649ce69da641432e571
Muhammad Abdullah Khan <abdullah.khan10032@gmail.com>
2024-08-10 23:38:17 +0500

    FIx tailwind CSS issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-
-function Home(props) {
-    return (
-        <div className='relative h-screen bg-cover bg-center' 
-        style ={{backgroundImage : "url('')"}}  >
-            <div className='absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center' >
-                <h1 className='bg-red-600 rounded-md '>Explore the World with Us</h1>
-                <p>Discover Amazing places at exclusive deals</p>
-            </div>
-        </div>
-    );
+function Home() {
+  return (
+    <div
+      className="relative h-screen bg-cover bg-center"
+      style={{ backgroundImage: "url('')" }}
+    >
+      <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center">
+        <h1 className="bg-red-600 rounded-md ">Explore the World with Us</h1>
+        <p>Discover Amazing places at exclusive deals</p>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
